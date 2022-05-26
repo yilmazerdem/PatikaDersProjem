86637e644937704cd2401a5e0305925b65a33a98
yilmazerdem <erdmylmz0@gmail.com>
2022-05-26 08:57:13 +0300

    Dik üçgen hesaplama

---
+++ src/MainProje/UcgenHesaplama.java
@@ -0,0 +1,18 @@
+package MainProje;
+import java.util.Scanner;
+public class UcgenHesaplama {
+    public static void main(String[] args) {
+        int uk,kk;
+        double h;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("KENAR UZUNLUKLARINI GİRİNİZ");
+        System.out.print("KISA KENAR : ");
+        kk=inp.nextInt();
+        System.out.print("UZUN KENAR : ");
+        uk= inp.nextInt();
+        h=Math.sqrt((kk*kk)+(uk*uk));
+
+        System.out.println("HIPOTENUS UZUN : "+ h);
+    }
+
+}
